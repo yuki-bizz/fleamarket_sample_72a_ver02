5089b2b4cbb8b5155bd14cd96d16de3d1bb86891
KenYoshinaga <dalzedarumin@gmail.com>
2020-06-06 21:23:30 +0900

    configuration02

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'displays/index'
-
+  
+  devise_for :users
+  resources :displays
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "displays#index"
 end
