134bb78745449d393d0fb8e55c4a652fae08c2cc
KenYoshinaga <dalzedarumin@gmail.com>
2020-06-07 05:53:39 +0900

    deploy_end

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'capistrano (3.1)'
+lock 'capistrano (3.14.0)'
 
 # Capistranoのログの表示に利用する
 set :application, 'fleamarket_sample_72a_ver02'
