22079eced80fbe6b21f3bbe6474e366742829b9a
yuki-bizz <yuki.bizz.8011@gmail.com>
2020-06-07 15:34:41 +0900

    static controller/model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200606121431) do
+ActiveRecord::Schema.define(version: 20200607062502) do
+
+  create_table "statics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
