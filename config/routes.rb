1c44a08e59fdedc57210c92854b35d1e43909b59
kazoe <officekazoe@gmail.com>
2020-06-10 19:10:07 +0900

    [WIP]クレジットカード登録
    # What
    ・カード画像UP
    ・カード登録ページ微調整
    ・component.scssのトップページエリアに親クラス追加
    ・不要scssファイル削除
    
    # Why
    ・チェック用として手元にmasterに画像を置いておく
    ・レイアウト調整が必要だったため
    ・.page_topクラスがないとクラス被りが出てレイアウト崩れ起きる可能性＆リファクタリングしずらい
    ・ファイル整理

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'}
+    
   resources :displays
+
+  # my_pageサイドメニューから確認用 add kazoe
+  get 'static/regist'
+  get 'static/card'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static#my_page"
 end
