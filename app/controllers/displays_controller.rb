9b8b3baa992c06107d76d1e2a80f67b8830167e0
natsuko819 <nkser_2319@icloud.com>
2020-06-22 20:36:29 +0900

    goods_items_list
    カテゴリー別・ブランド別での商品一覧ページの作成。（画像素材の表示は後ほど実装）

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -1,4 +1,14 @@
 class DisplaysController < ApplicationController
   def index
+    @goods_items = GoodsItem.all
+    pp @goods_items
+    #カテゴリー
+    @womens = GoodsItem.where(category: 'レディース')
+    @mens = GoodsItem.where(category: 'メンズ')
+    @toys = GoodsItem.where(category: 'おもちゃ・ホビー・グッズ')
+    @appliances = GoodsItem.where(category: '家電・スマホ・カメラ')
+    #ブランド
+    @apples = GoodsItem.where(brand: 'apple')
+    @louis_vuittons = GoodsItem.where(brand: 'Louis Vuitton')
   end
 end
