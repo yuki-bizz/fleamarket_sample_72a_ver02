ed72d5d3bd1cc85e5ee9f2978ecd0a1d6ddbaf48
kazoe <officekazoe@gmail.com>
2020-06-22 22:19:44 +0900

    [WIP]ユーザー登録
    # What
    電話番号半角数字のみ登録可能にするバリデーション追加
    
    # Why
    仕様書に準ずる

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
 
   validates :nickname,:family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :delivery_family_name, :delivery_first_name, :delivery_family_name_kana, :delivery_first_name_kana, :postcode, :province, :city, :address1, presence: true, uniqueness: true
 
+
+  validates :tellphone_number, format: {with: /\A[ぁ-んァ-ヶー－]+\z/,
+            message: "は半角数字のみが使えます"}
+
   #has_many :goods_items
   #has_many :orders, dependent: :destroy
   #has_one :card, dependent: :destroy
