578581556e886d506d11f99b1c02f2f28bff4ad8
kazoe <officekazoe@gmail.com>
2020-06-22 16:07:00 +0900

    [WIP]ユーザー登録
    # What
    基本セット
    
    # Why
    いつでも戻せるように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
-  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :delivery_family_name, :delivery_first_name, :delivery_family_name_kana, :delivery_first_name_kana, :postcode, :province, :city, :address1, :address2, :tellphone_number])
+  end
 
   private
 
@@ -25,13 +32,13 @@ class ApplicationController < ActionController::Base
   #    redirect_to new_user_session_url unless user_signed_in?
   #  end
 
-  def after_sign_in_path_for(resource)
-    root_path # ログイン後に遷移するpathを設定
-  end
+#  def after_sign_in_path_for(resource)
+#    root_path # ログイン後に遷移するpathを設定
+#  end
 
-  def after_sign_out_path_for(resource)
-    user_session_path # ログアウト後に遷移するpathを設定
-  end
+#  def after_sign_out_path_for(resource)
+#    user_session_path # ログアウト後に遷移するpathを設定
+#  end
 ############## devise周り add kazoe ##############
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :goods_items
-  has_many :orders, dependent: :destroy
-  has_one :card, dependent: :destroy
+
+  validates :nickname,:family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :delivery_family_name, :delivery_first_name, :delivery_family_name_kana, :delivery_first_name_kana, :postcode, :province, :city, :address1, presence: true, uniqueness: true
+
+  #has_many :goods_items
+  #has_many :orders, dependent: :destroy
+  #has_one :card, dependent: :destroy
 end
 
