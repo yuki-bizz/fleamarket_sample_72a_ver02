210ce14282fea8cad2f6d0da2e7c91cf94f75849
natsuko819 <nkser_2319@icloud.com>
2020-06-23 21:55:48 +0900

    goods_items_list
    aタグをlink_toに変更

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,6 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 #  root "displays#index"
-  root "static#regist"
+  root "displays#index"
 
 end
