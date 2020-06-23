8f832886ff7a1d4c91ad8acac44b28a725b4d4cf
kazoe <officekazoe@gmail.com>
2020-06-23 20:53:29 +0900

    [WIP]ユーザー登録
    # What
    deviseルート記述変更
    
    # Why
    エラー回避

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   
-  #devise_for :users
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'}
+  devise_for :users
+  #devise_for :users, controllers: {
+  #  registrations: 'users/registrations',
+  #  sessions: 'users/sessions'}
     
-  devise_scope :user do
-    get 'sign_in', :to => 'users/sessions#new'
-    get 'sign_out', :to => 'users/sessions#destroy'
-  end
+  #devise_scope :user do
+  #  get 'sign_in', :to => 'users/sessions#new'
+  #  get 'sign_out', :to => 'users/sessions#destroy'
+  #end
 
-  resources :displays
+  #resources :displays
 
   # トップページからの遷移 add ozaki
   get 'static/regist'
