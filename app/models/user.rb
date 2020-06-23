ff35b5e813fe8c6a67f4938418104615664feee3
kazoe <officekazoe@gmail.com>
2020-06-23 20:59:36 +0900

    [WIP]ユーザー登録
    # What
    バリデーション一部解除
    
    # Why
    登録可能にするため

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,24 +8,24 @@ class User < ApplicationRecord
   validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :delivery_family_name, :delivery_first_name, :delivery_family_name_kana, :delivery_first_name_kana, :postcode, :province, :city, :address1, presence: true, uniqueness: true
 
 
-  validates :family_name,
-  format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
-            message: "は全角のみが使えます"}
+  #validates :family_name,
+  #format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
+  #          message: "は全角のみが使えます"}
 
 
-  validates :first_name,
-  format: {with: /\A[a-zA-Z0-9]+\z/,
-            message: "は全角のみが使えます"}
+#  validates :first_name,
+#  format: {with: /\A[a-zA-Z0-9]+\z/,
+#            message: "は全角のみが使えます"}
 
-  validates :family_name_kana, 
-  format: {with: /\A[a-zA-Z0-9]+\z/,
-            message: "は全角のみが使えます"}
+#  validates :family_name_kana, 
+#  format: {with: /\A[a-zA-Z0-9]+\z/,
+#            message: "は全角のみが使えます"}
 
 
 # あとでunlessに直す
-  validates :tellphone_number,
-  format: {with: /\A[ぁ-んァ-ヶー－]+\z/,
-            message: "は半角数字のみが使えます"}
+#  validates :tellphone_number,
+#  format: {with: /\A[ぁ-んァ-ヶー－]+\z/,
+#            message: "は半角数字のみが使えます"}
 
 
 
