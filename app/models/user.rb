36e227a12da371c02dcb572102fda831cae9ecd0
kazoe <officekazoe@gmail.com>
2020-06-24 16:51:33 +0900

    [WIP]ユーザー登録
    # What
    １．jsバリデーションファイル追加
    ２．ログインなし状態での登録ページへのリダイレクト解除
    ３．deviseバリデーションコメントアウト
    ４．viewファイルへjsバリデーション追記
    ５．jsのheadへの読み込みの仕方変更
    
    # Why
    １．deviseでのバリデーションではなくjqueryにするため
    ２．仕様書に準ずる
    ３．バリデーションをjqueryにするため
    ４．バリデーションをjqueryにするため
    ５．バリデーションをjqueryにするため

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable #:validatable
 
 
-  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :delivery_family_name, :delivery_first_name, :delivery_family_name_kana, :delivery_first_name_kana, :postcode, :province, :city, :address1, presence: true, uniqueness: true
+  #validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :delivery_family_name, :delivery_first_name, :delivery_family_name_kana, :delivery_first_name_kana, :postcode, :province, :city, :address1, presence: true, uniqueness: true
 
 
   #validates :family_name,
