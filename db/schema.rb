313959236e27e8ee7b26f79cec9e2387c351a055
kazoe <officekazoe@gmail.com>
2020-06-25 17:14:51 +0900

    [WIP]ユーザー登録
    # What
    １．deviseエラーメッセージの見た目調整
    ２．deviseエラーメッセージコメントアウト削除
    ３．selectboxのオプションにバリューなしを追加
    ４．電話番号のカラム型変更
    
    # Why
    １．jsエラーと見た目を合わせる
    ２．deviseでのバリデーションがemail以外不要となった
    ３．空白のデータがDB保存されないように
    ４．型変更によるもの（int→str）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200621052729) do
+ActiveRecord::Schema.define(version: 20200624212327) do
 
   create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -98,7 +98,7 @@ ActiveRecord::Schema.define(version: 20200621052729) do
     t.string   "city",                                   null: false
     t.string   "address1",                               null: false
     t.string   "address2"
-    t.integer  "tellphone_number"
+    t.string   "tellphone_number"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
