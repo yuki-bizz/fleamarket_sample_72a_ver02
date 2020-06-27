5a40425c838c2345337e5678050efdbabb5851f9
yuki-bizz <yuki.bizz.8011@gmail.com>
2020-06-27 16:08:07 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   
-  #devise_for :users
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'}
+  devise_for :users
+  # devise_for :users, controllers: {
+  #   registrations: 'users/registrations',
+  #   sessions: 'users/sessions'}
     
-  devise_scope :user do
-    get 'sign_in', :to => 'users/sessions#new'
-    get 'sign_out', :to => 'users/sessions#destroy'
-  end
+  # devise_scope :user do
+  #   get 'sign_in', :to => 'users/sessions#new'
+  #   get 'sign_out', :to => 'users/sessions#destroy'
+  # end
 
   # resources :displays
   resources :goods_items, only: [:show, :destroy, :edit]
