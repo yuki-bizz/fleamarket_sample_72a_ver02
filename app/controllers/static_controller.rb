18fc166314ab813ae3ef7bc788740c713c0a6714
yuki-bizz <yuki.bizz.8011@gmail.com>
2020-06-28 15:47:52 +0900

    haml

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,12 +1,18 @@
 class StaticController < ApplicationController
-
-  def destroy
-    goods_item = Goods_item.find(params[:id])
-    goods_item.destroy
-  end
-
-  def edit
-    @goods_item = Goods_item.find(params[:id])
-  end
   
+# チーム内のテスト用としてコメントアウトして残しておきます。内容はgoods_items_controllerと同じ
+  # def show
+  #   @goods_item = GoodsItem.find(params[:id])
+  # end
+
+  # def destroy
+  #   goods_item = GoodsItem.find(params[:id])
+  #   # binding.pry
+  #   goods_item.destroy
+  # end
+
+  # def edit
+  #   @goods_item = GoodsItem.find(params[:id])
+  # end
+
 end
