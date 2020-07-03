8d48f37225136aad8b6c542038d69f97b2fd5d62
yuki-bizz <yuki.bizz.8011@gmail.com>
2020-07-03 22:02:44 +0900

    edit_page

--- app/controllers/goods_items_controller.rb
+++ app/controllers/goods_items_controller.rb
@@ -1,18 +1,26 @@
 class GoodsItemsController < ApplicationController
-
-before_action :set_goods_item, except: [:index, :new, :create]
+  before_action :authenticate_user!
+  before_action :set_goods_item, except: [:index, :new, :create]
 
 # 一覧画面（確認用）
   def index
-    @goods_item = GoodsItem.includes(:images).order('created_at DESC')
+    @goods_items = GoodsItem.includes(:images).order('created_at DESC')
   end
 
 # 商品出品画面
   def new
+    redirect_to new_user_session_path unless user_sined_in?
+    @goods_item = GoodsItem.new
+    @goods_item.images.new
   end
 
 # 商品保存機能
   def create
+    @goods_item = GoodsItem.new(goods_item_params)
+    if @goods_item.save
+      redirect_to goods_items_path
+    else
+      render :new
   end
 
 # 商品更新機能
@@ -40,11 +48,15 @@ before_action :set_goods_item, except: [:index, :new, :create]
   private
 
   def goods_item_params
-    params.require(:goods_item).permit(:name, :price, images_attributes:  [:src, :_destroy, :id])
+    params.require(:goods_item).permit(:name, :price, :explanation, :brand, :condition_id, :shipping_fee [:src, :_destroy, :id])
   end
  
   def set_goods_item
     @goods_item = GoodsItem.find(params[:id])
+  end
+
+  def redirect_root
+    redirect_to root_path unless user_sined_in?
   end
 
 end
