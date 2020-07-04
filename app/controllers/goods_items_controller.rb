c54fdcd5c2a78b48cf45251051dfeac5450ddc8e
yuki-bizz <yuki.bizz.8011@gmail.com>
2020-07-04 09:40:56 +0900

    Update goods_items_controller.rb

--- app/controllers/goods_items_controller.rb
+++ app/controllers/goods_items_controller.rb
@@ -2,46 +2,45 @@ class GoodsItemsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_goods_item, except: [:index, :new, :create]
 
-# 一覧画面（確認用）
-  def index
-    @goods_items = GoodsItem.includes(:images).order('created_at DESC')
-  end
+#  # 一覧画面（確認用）
+#   def index
+#     @goods_items = GoodsItem.includes(:images).order('created_at DESC')
+#   end
 
-# 商品出品画面
-  def new
-    redirect_to new_user_session_path unless user_sined_in?
-    @goods_item = GoodsItem.new
-    @goods_item.images.new
-  end
+#  # 商品出品画面
+#   def new
+#     redirect_to new_user_session_path unless user_sined_in?
+#     @goods_item = GoodsItem.new
+#     @goods_item.images.new
+#   end
 
-# 商品保存機能
-  def create
-    @goods_item = GoodsItem.new(goods_item_params)
-    if @goods_item.save
-      redirect_to goods_items_path
-    else
-      render :new
-  end
+#  # 商品保存機能
+#   def create
+#     @goods_item = GoodsItem.new(goods_item_params)
+#     if @goods_item.save
+#       redirect_to goods_items_path
+#     else
+#       render :new
+#   end
 
-# 商品更新機能
-  def update
-    if @goods_item.update(goods_item_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
+#  # 商品更新機能
+#   def update
+#     if @goods_item.update(goods_item_params)
+#       redirect_to root_path
+#     else
+#       render :edit
+#   end
 
-# 商品詳細ページ
+ # 商品詳細ページ
   def show
   end
 
-# 商品削除機能
+ # 商品削除機能
   def destroy
     goods_item.destroy
   end
 
-# 商品編集画面
+ # 商品編集画面
   def edit
   end
 
@@ -55,8 +54,7 @@ class GoodsItemsController < ApplicationController
     @goods_item = GoodsItem.find(params[:id])
   end
 
-  def redirect_root
-    redirect_to root_path unless user_sined_in?
-  end
-
+  # def redirect_root
+  #   redirect_to root_path unless user_sined_in?
+  # end
 end
