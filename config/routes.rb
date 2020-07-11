8c0ddac584491e64a3373d15af4a15e203717658
yuki-bizz <yuki.bizz.8011@gmail.com>
2020-07-11 16:21:09 +0900

    pull master

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
 
   # resources :displays
   # resources :goods_items, except: :index
+  # resources :goods_items
 
   resources :goods_items do
       collection do 
