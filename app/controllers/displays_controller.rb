8f923f6131264d00349f5b0c0f1ca1cc713714ab
ShotaroTsutsumi <shotaro71@gmail.com>
2020-07-12 17:11:48 +0900

    card-purchase-function

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -3,7 +3,7 @@ class DisplaysController < ApplicationController
     #N＋１問題解消
     @goods_items = GoodsItem.all.includes(user: :images)
     #カテゴリー
-    @ladys = GoodsItem.where(category_id: '1')
+    @ladies = GoodsItem.where(category_id: '1')
     @mens = GoodsItem.where(category_id: '2')
     @toy_hobby_goods = GoodsItem.where(category_id: '6')
     @appliance_smartphone_cameras = GoodsItem.where(category_id: '8')
