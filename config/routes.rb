ec6a29e318e51ba1e034c7d972cbdefde30ddad5
kazoe <officekazoe@gmail.com>
2020-07-23 23:45:28 +0900

    [WIP]SNSユーザー登録
    # What
    必要ファイル一式作成
    
    # Why
    ユーザー登録を楽にするため

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,10 @@ Rails.application.routes.draw do
 
   get 'cards/show'
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+
+  #devise_for :users
   # devise_for :users, controllers: {
   #   registrations: 'users/registrations',
   #   sessions: 'users/sessions'}
