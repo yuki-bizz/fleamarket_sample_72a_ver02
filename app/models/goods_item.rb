86b1d1baf659a30fd0d279725fb5519a6e181de7
yuki-bizz <yuki.bizz.8011@gmail.com>
2020-07-25 00:50:30 +0900

    comment update

--- app/models/goods_item.rb
+++ app/models/goods_item.rb
@@ -5,7 +5,8 @@ class GoodsItem < ApplicationRecord
   has_many :images, dependent: :destroy
   ## 購入機能実装時使用 ###
   has_one :order, dependent: :destroy
-  
+  ## コメント機能で使用 ##
+  has_many :comments
 
   
   # モデルimage.rbにバリデーションをかける
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   #has_many :orders, dependent: :destroy
   #has_one :card, dependent: :destroy
 
+  has_many :comments
 end
