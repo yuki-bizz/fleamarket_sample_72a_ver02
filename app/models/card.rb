c6606d45f98a54eb134f6adbeef32eb039077286
ShotaroTsutsumi <shotaro71@gmail.com>
2020-07-25 11:52:13 +0900

    revision

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,7 @@
 class Card < ApplicationRecord
   belongs_to :user
 
-  validates :user, presence: true
+  # validates :user, presence: true
   validates :customer_id, presence: true
   validates :card_id, presence: true
 
