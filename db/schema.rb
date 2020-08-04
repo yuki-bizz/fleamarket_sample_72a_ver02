06d52a2486227f63190d4ef9ca629c1834f42a19
KenYoshinaga <dalzedarumin@gmail.com>
2020-08-04 22:11:27 +0900

    調整
    active_hashのデータ調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20200802020413) do
-
-ActiveRecord::Schema.define(version: 20200726114014) do
-
 
   create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "user_id"
