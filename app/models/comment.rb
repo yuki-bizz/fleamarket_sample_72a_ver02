f4cce1f57dad498b3547634af2e8d6174fc90404
yuki-bizz <yuki.bizz.8011@gmail.com>
2020-08-07 21:22:13 +0900

    修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :goods_item, foreign_key: "user_id", dependent: :destroy
-  belongs_to :user, foreign_key: "goods_item_id", dependent: :destroy
+  belongs_to :goods_item, foreign_key: "goods_item_id"
+  belongs_to :user, foreign_key:  "user_id"
 end
